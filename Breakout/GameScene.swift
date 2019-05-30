474d4e982616579f648aad2d8de53f1d13b057a5
Akroatis <patrick.stacey2000@gmail.com>
2019-05-30 15:41:44 -0500

    Added programmatic ball that bounces on screen border collision.

--- Breakout/GameScene.swift
+++ Breakout/GameScene.swift
@@ -19,13 +19,14 @@ struct PhysicsCategory {
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    var ball = SKSpriteNode(imageNamed: "ball1")
+    
     let paddle = SKSpriteNode(imageNamed: "paddle1")
+    let ball = SKShapeNode(circleOfRadius: 20 ) // Size of Ball
     
     override func didMove(to view: SKView) {
         backgroundColor = SKColor.white
-        ball.position = CGPoint(x: size.width * 0.5, y: size.height * 0.2)
-        addChild(ball)
+//        ball.position = CGPoint(x: size.width * 0.5, y: size.height * 0.2)
+//        addChild(ball)
         paddle.position = CGPoint(x: size.width * 0.5, y: size.height * 0.1)
         addChild(paddle)
         
@@ -39,15 +40,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         self.physicsBody = borderBody
         
         physicsWorld.gravity = CGVector(dx: 0.0, dy: 0.0)
-        ball.physicsBody?.applyImpulse(CGVector(dx: 2.0, dy: -2.0))
-        ball.physicsBody?.isDynamic = true
         
-//        let actualDuration = random(min: CGFloat(2.0), max: CGFloat(4.0))
-//        let actionMove = SKAction.move(to: CGPoint(x: -enemy.size.width / 2, y: actualY), duration: TimeInterval(actualDuration))
-//        let actionMoveDone = SKAction.removeFromParent()
-//        enemy.run(SKAction.sequence([actionMove, actionMoveDone]))
+        //makes ball
+        
+        ball.position = CGPoint(x: size.width * 0.5, y: size.height * 0.2)
+        ball.strokeColor = SKColor.orange
+        ball.glowWidth = 1.0
+        ball.fillColor = SKColor.cyan
+        self.addChild(ball)
+        //Ball Physics
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+        ball.physicsBody?.affectedByGravity = false
+        ball.physicsBody?.isDynamic = true
+        ball.physicsBody?.restitution = 1
+        ball.physicsBody?.linearDamping = 0
+        ball.physicsBody?.angularDamping = 0
+        ball.physicsBody?.friction = 0
+        
         
     }
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let initForce = CGVector(dx: 20.0, dy: 40.0)
+        ball.physicsBody?.applyImpulse(initForce)
+    }
+    
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         //setup paddle
